7c5b1c7319aa6f9d09ebfb9ca76d9e210a598ac0
Alec Helbling <alechelbling@gatech.edu>
2019-02-17 17:12:44 -0500

    Setup layout xml file for the player configuration page. I need to connect everything to the model now.

--- app/src/main/java/com/example/spacetrader/views/PlayerConfigurationPage.java
+++ app/src/main/java/com/example/spacetrader/views/PlayerConfigurationPage.java
@@ -2,15 +2,38 @@ package com.example.spacetrader.views;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.spacetrader.R;
 
+import java.util.EventListener;
+
 public class PlayerConfigurationPage extends Activity {
+
+    private int pilotPoints;
+    private int fighterPoints;
+    private int traderPoints;
+    private int engineerPoints;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_configuration);
     }
 
+    public void increaseInteger(View view) {
+        minteger = minteger + 1;
+        display(minteger);
+
+    }public void decreaseInteger(View view) {
+        minteger = minteger - 1;
+        display(minteger);
+    }
+
+    private void display(int number) {
+        TextView displayInteger = (TextView) findViewById(
+                R.id.integer_number);
+        displayInteger.setText("" + number);
+    }
 
 }
