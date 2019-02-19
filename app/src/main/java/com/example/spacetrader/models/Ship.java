8793862a6b14076aa958d801ab4b578b19268e97
Justin Wurst <jmwurst@me.com>
2019-02-19 13:08:08 -0500

    main menu reworked

--- app/src/main/java/com/example/spacetrader/models/Ship.java
+++ app/src/main/java/com/example/spacetrader/models/Ship.java
@@ -2,7 +2,7 @@ package com.example.spacetrader.models;
 
 public class Ship {
     /*
-        This is an empty Ship class becuase there
+        This is an empty Ship class because there
         were not directions as to how this should be implemented.
      */
 }
