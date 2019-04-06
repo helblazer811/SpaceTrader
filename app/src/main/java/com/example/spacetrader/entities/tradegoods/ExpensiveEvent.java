6608a9cf4a84a62ae66e54e69cfd4d792e5034eb
maojoejoe <maojoejoe@gmail.com>
2019-04-06 16:43:21 -0400

    Added the CS2340.xml for usage in Analyze>Inspect Code>change profile with gear

--- app/src/main/java/com/example/spacetrader/entities/tradegoods/ExpensiveEvent.java
+++ app/src/main/java/com/example/spacetrader/entities/tradegoods/ExpensiveEvent.java
@@ -1,12 +1,15 @@
 package com.example.spacetrader.entities.tradegoods;
-
+/**
+ * ExpensiveEvent is an enum with instances of planet characteristics that can affect
+ * prices upwards
+ */
 public enum ExpensiveEvent {
     DESERT(0),
     LIFELESS(1),
     POORSOIL(2),
     MINERALPOOR(3);
 
-    private Integer code;
+    private final Integer code;
 
     ExpensiveEvent(Integer code){
         this.code = code;
