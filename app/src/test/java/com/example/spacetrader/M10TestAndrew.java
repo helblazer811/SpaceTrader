4ea7ccb68ee6a98ceedae4bde0509301a6226913
Andrew Wang <andrewatga@gmail.com>
2019-04-08 19:44:22 -0400

    Fixed Junit test

--- app/src/test/java/com/example/spacetrader/M10TestAndrew.java
+++ app/src/test/java/com/example/spacetrader/M10TestAndrew.java
@@ -1,8 +1,16 @@
 package com.example.spacetrader;
+import com.example.spacetrader.entities.ship.Ship;
+import com.example.spacetrader.entities.ship.ShipType;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class M10TestAndrew {
 
     @Test
+    public void testTakeDamage() {
+        Ship s = new Ship(ShipType.FIREFLY);
+        s.takeDamage(10);
+        assertEquals(90, s.getShipHealth());
+    }
 }
