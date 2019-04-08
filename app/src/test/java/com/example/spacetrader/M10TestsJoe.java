0ede9c4651e7dea99fd1f8c2981a0f3a9e31b9e6
maojoejoe <maojoejoe@gmail.com>
2019-04-08 19:31:27 -0400

    hopefully this doesnt break yall

--- app/src/test/java/com/example/spacetrader/M10TestsJoe.java
+++ app/src/test/java/com/example/spacetrader/M10TestsJoe.java
@@ -2,9 +2,27 @@ package com.example.spacetrader;
 
 import org.junit.Test;
 
+import com.example.spacetrader.entities.Inventory;
+import com.example.spacetrader.entities.tradegoods.TradeGood;
+import com.example.spacetrader.*;
+
+import java.util.HashMap;
+
 public class M10TestsJoe {
+//    private HashMap<TradeGood, Integer> aMap;
+
     @Test
     public void check_put() {
-
+        Inventory testInv = new Inventory(10);
+//        testInv.put(TradeGood.WATER,1);
+//        testInv.put(TradeGood.FURS,1);
+//        testInv.put(TradeGood.FOOD,1);
+//        testInv.put(TradeGood.ORE,1);
+//        testInv.put(TradeGood.GAMES,1);
+//        testInv.put(TradeGood.FIREARMS,1);
+//        testInv.put(TradeGood.MEDICINE,1);
+//        testInv.put(TradeGood.MACHINES,1);
+//        testInv.put(TradeGood.NARCOTICS,1);
+//        testInv.put(TradeGood.ROBOTS,1);
     }
 }
