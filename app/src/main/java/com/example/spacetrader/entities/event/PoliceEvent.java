1331f80df259c8a484bea3cd09e80842c08e8c1e
Jared Carbone <32436031+jaredcarbone2@users.noreply.github.com>
2019-04-07 23:30:35 -0400

    added java docs to multiple classes in the event folder

--- app/src/main/java/com/example/spacetrader/entities/event/PoliceEvent.java
+++ app/src/main/java/com/example/spacetrader/entities/event/PoliceEvent.java
@@ -4,8 +4,15 @@ import com.example.spacetrader.entities.Player;
 import com.example.spacetrader.views.maingame.event.PirateActivity;
 import com.example.spacetrader.views.maingame.event.PoliceActivity;
 
+/**
+ * Triggers whether or not police appear for the player
+ */
 public class PoliceEvent extends RandomEvent {
-
+    /**
+     * constructor for police events
+     * @param chance for event to occur
+     * @param player player that event is happening to
+     */
     public PoliceEvent(float chance, Player player) {
         super(chance, PoliceActivity.class, player);
     }
--- app/src/main/java/com/example/spacetrader/entities/event/RandomEvent.java
+++ app/src/main/java/com/example/spacetrader/entities/event/RandomEvent.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 
 import com.example.spacetrader.entities.Player;
 
+/**
+ * Abstract class for random event
+ */
 public abstract class RandomEvent {
 
     private boolean happens;
@@ -13,28 +16,54 @@ public abstract class RandomEvent {
     private Class eventActivityClass;
     private Player player;
 
+    /**
+     *  constructor for a random event
+     * @param chance odds for random event
+     * @param eventActivityClass activity class for the random event
+     * @param player the player object
+     */
     public RandomEvent(float chance, Class eventActivityClass, Player player) {
         this.chance = chance;
         this.eventActivityClass = eventActivityClass;
         this.player = player;
     }
 
+    /**
+     * getter or chance
+     * @return chance value of random event
+     */
     public float getChance() {
         return chance;
     }
 
+    /**
+     * setter for event
+     * @param chance chance value of random event
+     */
     public void setChance(float chance) {
         this.chance = chance;
     }
 
+    /**
+     * getter for player
+     * @return player object
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * setter for player
+     * @param player player object
+     */
     public void setPlayer(Player player) {
         this.player = player;
     }
 
+    /**
+     * method to determine the chance an event happening
+     * @return whether or not the random event actually happens
+     */
     public boolean happens() {
         if (decided == false) {
             this.happens = Math.random() < chance;
@@ -43,14 +72,26 @@ public abstract class RandomEvent {
         return this.happens;
     }
 
+    /**
+     * getter for eventActivityClass
+     * @return event activity class
+     */
     public Class getEventActivityClass() {
         return eventActivityClass;
     }
 
+    /**
+     * setter for event activity class
+     * @param eventActivityClass
+     */
     public void setEventActivityClass(Class eventActivityClass) {
         this.eventActivityClass = eventActivityClass;
     }
 
+    /**
+     * performs the event if the the chance allows
+     * @param application the event happening in the game
+     */
     public void perform(Application application) {
         if (happens()) {
             //performs the actual event
