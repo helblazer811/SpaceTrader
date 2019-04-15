3124f0e36cee2d98354a35c00c7cc2433ba55531
helblazer811 <maojoejoe@gmail.com>
2019-04-15 00:01:14 -0400

    Wrote unit testing

---
+++ app/src/test/java/com/example/spacetrader/M10TestAlec.java
@@ -0,0 +1,40 @@
+package com.example.spacetrader;
+
+import org.junit.Test;
+
+import com.example.spacetrader.entities.event.Enemy;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void damageDealtCheck() throws Exception {
+        Enemy testEnemy = new Enemy(100, 100, 10);
+        testEnemy.doDamage(10);
+        assertEquals(testEnemy.getCurrentHealth(), 90);
+        testEnemy.doDamage(50);
+        assertEquals(testEnemy.getCurrentHealth(), 40);
+    }
+
+    @Test
+    public void healthEdgeCase() throws Exception {
+        Enemy testEnemy = new Enemy(100, 100, 10);
+        testEnemy.doDamage(50);
+        testEnemy.doDamage(100);
+        assertEquals(testEnemy.getCurrentHealth(), 0);
+
+    }
+
+    @Test
+    public void combinedTest() throws Exception {
+        Enemy testEnemy = new Enemy(100, 100, 10);
+        testEnemy.doDamage(50);
+        assertEquals(testEnemy.getCurrentHealth(), 50);
+        testEnemy.doDamage(49);
+        assertEquals(testEnemy.getCurrentHealth(), 1);
+        testEnemy.doDamage(2);
+        assertEquals(testEnemy.getCurrentHealth(), 0);
+    }
+
+}
